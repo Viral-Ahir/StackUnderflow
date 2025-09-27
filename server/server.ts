463545064bf8b7e62317917b1987e6d63c4e6f81
Viral-Ahir <ahirviral01@gmail.com>
2025-09-27 14:17:09 -0400

    Server Changes vercel

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,7 @@
 import express, { Express } from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import { Server } from "http";
+// import { Server } from "http";
 import dotenv from "dotenv";
 import authRoutes from "./controller/auth"; // Import auth routes
 import answerController from "./controller/answer";
@@ -17,7 +17,7 @@ const MONGO_URI: string =
   process.env.MONGO_URI || "mongodb://127.0.0.1:27017/stackUnderflow";
 const CLIENT_URL: string = process.env.CLIENT_URL || "http://localhost:3000";
 console.log(CLIENT_URL);
-const port: number = parseInt(process.env.PORT || "8000", 10);
+// const port: number = parseInt(process.env.PORT || "8000", 10);
 mongoose.connect(MONGO_URI);
 
 const app: Express = express();
@@ -58,27 +58,28 @@ app.use("/comment", commentRoutes);
 app.use("/vote", votesRoutes);
 
 // Start the server and assign it to the 'server' variable
-const server: Server = app.listen(port, () => {
-  console.log(`Server starts at http://localhost:${port}`);
-});
+// const server: Server = app.listen(port, () => {
+//   console.log(`Server starts at http://localhost:${port}`);
+// });
 
 /**
  * Gracefully close the server and the database connection
  */
-process.on("SIGINT", () => {
-  server.close(() => {
-    console.log("Server closed.");
-  });
-  mongoose
-    .disconnect()
-    .then(() => {
-      console.log("Database instance disconnected.");
-      process.exit(0);
-    })
-    .catch((err) => {
-      console.error("Error during disconnection:", err);
-      process.exit(1);
-    });
-});
+// process.on("SIGINT", () => {
+//   server.close(() => {
+//     console.log("Server closed.");
+//   });
+//   mongoose
+//     .disconnect()
+//     .then(() => {
+//       console.log("Database instance disconnected.");
+//       process.exit(0);
+//     })
+//     .catch((err) => {
+//       console.error("Error during disconnection:", err);
+//       process.exit(1);
+//     });
+// });
 
-module.exports = server;
+// module.exports = server;
+export default app;
